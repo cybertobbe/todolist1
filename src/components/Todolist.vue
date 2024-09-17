658b70d4c10a7e771033cee35ac3660381803fb4
cybertobbe <tobias.sjostrom@iths.se>
2024-09-17 18:24:41 +0200

    Refactor, rename isDone()

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -4,9 +4,9 @@ import IconTodo from "@/components/icons/IconTodo.vue";
 
 const header = ref('My ToDo-list')
 const items = ref([
-  { id: 1, label: "Frukost", isGreen: false },
-  { id: 2, label: "Träna", isGreen: false },
-  { id: 3, label: "Programmera", isGreen: false }
+  { id: 1, label: "Frukost", isDone: false },
+  { id: 2, label: "Träna", isDone: false },
+  { id: 3, label: "Programmera", isDone: false }
 ])
 
 const newTodo = ref("")
@@ -34,7 +34,7 @@ const removeAll = () => {
 }
 
 const done = (item) => {
-  item.isGreen = !item.isGreen
+  item.isDone = !item.isDone
 }
 </script>
 
@@ -58,7 +58,7 @@ const done = (item) => {
         </div>
 
         <ul class="todo-list">
-          <li v-for="(item, index) in items" :key="item.id" @click="done(item)" :class="{ green: item.isGreen }">
+          <li v-for="(item, index) in items" :key="item.id" @click="done(item)" :class="{ done: item.isDone }">
             <div class="todo-item">
               <span>{{ item.label }}</span>
               <button @click.stop="remove(item)">Remove</button>
@@ -71,92 +71,8 @@ const done = (item) => {
 </template>
 
 <style scoped>
-.container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  background-color: #f9f9f9;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-header {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-}
-
-.icon-text {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  color: #007bff;
-}
-
-.icon-text svg {
-  margin-right: 10px;
-}
-
-.main-area {
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  color: #333;
-}
-
-.input-area {
-  display: flex;
-  gap: 10px;
-  margin-bottom: 20px;
-}
-
-input {
-  flex: 1;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-button {
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  background-color: #007bff;
-  color: #fff;
-  cursor: pointer;
-}
-
-button:hover {
-  background-color: #0056b3;
-}
-
-.todo-list {
-  list-style: none;
-  padding: 0;
-}
-
-.todo-item {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  margin-bottom: 10px;
-  background-color: #f1f1f1;
-}
-
-.todo-item:hover {
-  background-color: #e9e9e9;
-}
-
-.green {
-  color: DBE6EA;
-}
+  .done {
+    color: #b3b4b8;
+    text-decoration: line-through;
+  }
 </style>
